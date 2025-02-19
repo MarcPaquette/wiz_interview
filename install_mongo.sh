2941ba50e566b4ed703fcdc43c49423aa8b00e38
Marc Paquette <marcphilippaquette@gmail.com>
2025-02-19 14:41:03 -0500

    Move script location

--- install_mongo.sh
+++ install_mongo.sh
@@ -5,4 +5,4 @@ MONGODB_HOST=$(terraform output -raw mongodb_host)
 MONGO_DB_ZONE=$(terraform output -raw mongodb_zone)
 gcloud compute ssh "$MONGODB_HOST" \
    --zone "$MONGO_DB_ZONE" \
-   --command "curl https://raw.githubusercontent.com/MarcPaquette/wiz_interview/refs/heads/main/mongo_deploy.sh | bash" 
+   --command "curl https://raw.githubusercontent.com/MarcPaquette/wiz_interview/refs/heads/main/subscripts/mongo_deploy.sh | bash"
